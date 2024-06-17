768d447a0106a041fd62f95b5e653cbade621f10
Nattydodo <satirathai.n@gmail.com>
2024-06-17 15:11:45 +0700

    Update 6/17/2024 (3)

--- lab2-3.py
+++ lab2-3.py
@@ -1,16 +1,26 @@
-H = float(input("Hight(cm) = "))
-W = float(input("Wight(kg) = "))
+H = (input("Hight(cm) = "))
 
-BMI = float(W/((H/100)**2))
+if H.isnumeric():
 
-print("BMI = " + "%.2f" % BMI)
-if BMI>30:
-    print("อ้วนมาก/โรคอ้วนระดับ 3")
-elif BMI>25 and BMI<29.90:
-    print("อ้วน/โรคอ้วนระดับ 2")
-elif BMI>23 and BMI<24.90:
-    print("ท้วม/โรคอ้วนระดับ 1")
-elif BMI>18.50 and BMI<22.90:
-    print("ปกติ (สุขภาพดี)")
+    W = (input("Wight(kg) = "))
+
+    if W.isnumeric():
+
+        BMI = float(float(W)/((float(H)/100)**2))
+
+        print("BMI = " + "%.2f" % BMI)
+        if BMI>30:
+            print("อ้วนมาก/โรคอ้วนระดับ 3")
+        elif BMI>25 and BMI<29.90:
+            print("อ้วน/โรคอ้วนระดับ 2")
+        elif BMI>23 and BMI<24.90:
+            print("ท้วม/โรคอ้วนระดับ 1")
+        elif BMI>18.50 and BMI<22.90:
+            print("ปกติ (สุขภาพดี)")
+        else:
+            print("น้ำหนักน้อย/ผอม")
+
+    else:
+        print("โปรดใส่ข้อมูลตัวเลข")
 else:
-    print("น้ำหนักน้อย/ผอม")
+    print("โปรดใส่ข้อมูลตัวเลข")
